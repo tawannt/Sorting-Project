8c429baa2ae1b3e9caa4b3b5c8460651c47cbdc8
ndthanh308 <ndthanh308@gmail.com>
2024-06-24 22:49:39 +0700

    done command 4

--- cmd.h
+++ cmd.h
@@ -5,7 +5,7 @@ int *readFile(string fileName, int &size);
 void writeFile(string fileName, int *arr, int size);
 void selectAlgorithm(void (*&findRunTime)(int *&, int, long long &), void (*&countComparisons)(int *&, int, long long &), string algorithmName);
 void Generate(string inputOrder, int *&arr, int inputSize);
-void Analysis(void (*findRunTime)(int *&, int, long long &), void (*countComparisons)(int *&, int, long long &), int *&arr, int size, string output_parameter, long long &time, long long &count_comparison);
+void Do(void (*findRunTime)(int *&, int, long long &), void (*countComparisons)(int *&, int, long long &), int *&arr, int size, string output_parameter, long long &time, long long &count_comparison);
 
 
 void command_4(string algorithmName_1, string algorithmName_2, string filenameInput,  int &size);
--- commandLine.cpp
+++ commandLine.cpp
@@ -7,7 +7,7 @@ int *readFile(string fileName, int &size)
     ifstream ifs(fileName);
     if(!ifs.is_open())
     {
-        cout << "Cannot open input file!\n";
+        cout << "Cannot open input file!!!\n";
         return NULL;
     }
 
@@ -29,7 +29,7 @@ void writeFile(string fileName, int *arr, int size)
     ofstream ofs(fileName);
     if(!ofs.is_open())
     {
-        cout << "Cannot open input file!\n";
+        cout << "Cannot open input file!!!\n";
         return;
     }
     ofs << size << endl;
@@ -42,11 +42,11 @@ void writeFile(string fileName, int *arr, int size)
 
 void printOptions()
 {
-    cout << "Command 1: [Execution file] -a [Algorithm] [Given input] [Output parameters(s)]" << endl;
-    cout << "Command 2: [Execution file] -a [Algorithm] [Input size] [Input order] [Output parameters(s)]" << endl;
-    cout << "Command 3: [Execution file] -a [Algorithm] [Input size] [Output parameters(s)]" << endl;
-    cout << "Command 4: [Execution file] -c [Algorithm 1] [Algorithm 2] [Given input]" << endl;
-    cout << "Command 5: [Execution file] -c [Algorithm 1] [Algorithm 2] [Input size] [Input order]" << endl;
+    cout << "Command 1: [Execution file] -a [Algorithm] [Given input] [Output parameters(s)].\n";
+    cout << "Command 2: [Execution file] -a [Algorithm] [Input size] [Input order] [Output parameters(s)].\n";
+    cout << "Command 3: [Execution file] -a [Algorithm] [Input size] [Output parameters(s)].\n";
+    cout << "Command 4: [Execution file] -c [Algorithm 1] [Algorithm 2] [Given input].\n";
+    cout << "Command 5: [Execution file] -c [Algorithm 1] [Algorithm 2] [Input size] [Input order].\n";
 }
 
 void selectAlgorithm(void (*&findRunTime)(int *&, int, long long &), void (*&countComparisons)(int *&, int, long long &), string algorithmName)
@@ -129,7 +129,7 @@ void Generate(string inputOrder, int *&arr, int inputSize)
     }
 }
 
-void Analysis(void (*findRunTime)(int *&, int, long long &), void (*countComparisons)(int *&, int, long long &), int *&arr, int size, string output_parameter, long long &time, long long &count_comparison)
+void Do(void (*findRunTime)(int *&, int, long long &), void (*countComparisons)(int *&, int, long long &), int *&arr, int size, string output_parameter, long long &time, long long &count_comparison)
 {
     if (output_parameter == "-time")
     {
@@ -158,5 +158,55 @@ void Analysis(void (*findRunTime)(int *&, int, long long &), void (*countCompari
 
 void command_4(string algorithmName_1, string algorithmName_2, string filenameInput, int &size)
 {
-    
+    int *arr = readFile(filenameInput, size);
+    if(arr != NULL)
+    {
+        // initialize function pointer
+        void (*findRunTime)(int *&, int, long long &) = NULL;
+        void (*countComparisons)(int *&, int, long long &) = NULL;
+
+        // initialize run_time_1, 2 and count_comparisons_1,2
+        long long time_1 = 0, time_2 = 0, cnt_1 = 0, cnt_2 = 0; 
+        // initialize = 0 just for case cannot run the program, when cout wiil not error and we will know if programe had been done or not
+
+        // do for algorithm 1
+        // find algorithm 1
+        selectAlgorithm(findRunTime, countComparisons, algorithmName_1);
+        if(findRunTime && countComparisons) // if algorithmName_1 is in set
+        {
+            Do(findRunTime, countComparisons, arr, size, "-both", time_1, cnt_1);
+        }
+        else
+        {
+            cout << algorithmName_1 << " is not in set!!!\n";
+            delete[] arr;
+            return;
+        }
+
+        //  do for algorithm 2
+        // reinitialize pointer function as NULL
+        findRunTime = NULL;
+        countComparisons = NULL;
+
+        selectAlgorithm(findRunTime, countComparisons, algorithmName_2);
+        if(findRunTime && countComparisons)
+        {
+            Do(findRunTime, countComparisons, arr, size, "-both", time_2, cnt_2);
+        }
+        else
+        {
+            cout << algorithmName_2 << " is not in set!!!\n";
+            delete[] arr;
+            return;
+        }
+
+        cout << "COMPARE MODE\n";
+        cout << "Algorithm: " << algorithmName_1 << " | " << algorithmName_2 << '\n';
+        cout << "Input file: " <<  filenameInput << '\n';
+        cout << "Input size: " << size << '\n'; 
+        cout << "----------------------------\n";
+        cout << "Running time: " << time_1 << " | " << time_2 << '\n';
+        cout << "Comparisons: " << cnt_1 << " | " << cnt_2 << '\n';
+
+    }
 }
